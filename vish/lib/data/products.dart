6445d2c34e63f2ae3776f709b72bf5d08b123be0
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-10-10 23:50:33 -0300

    feat-bottomNavBar added

---
+++ vish/lib/data/products.dart
@@ -0,0 +1,58 @@
+import '../models/product.dart';
+
+final List<Product> products = [
+  Product(
+      description:
+          "Maçã gala nacional, produzida nas terras fantásticas. Nutritiva e suculenta.",
+      id: "1",
+      name: "Maçã Gala Nacional",
+      measure: "1Kg",
+      imageUrl:
+          "https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/maca-gala1-1e5fb8f95f2e6d251a15221697061927-1024-1024.jpg",
+      price: 4.99),
+  Product(
+      description:
+          "Banana prata, produzida nas terras fantásticas. Nutritiva e suculenta.",
+      id: "2",
+      name: "Banana Prata",
+      measure: "1Kg",
+      imageUrl:
+          "https://ceagesp.gov.br/wp-content/uploads/2019/12/Banana_pratapng.png",
+      price: 3.99),
+  Product(
+      description:
+          "Uva sem semente, produzida nas terras fantásticas. Nutritiva e suculenta.",
+      id: "3",
+      name: "Uva sem semente",
+      measure: "1Kg",
+      imageUrl:
+          "https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/uva-brs-isis-sem-semente1-4117fb3e4898b50a5a16102264065112-1024-1024.jpg",
+      price: 7.99),
+  Product(
+      description:
+          "Maçã gala nacional, produzida nas terras fantásticas. Nutritiva e suculenta.",
+      id: "1",
+      name: "Maçã Gala Nacional",
+      measure: "1Kg",
+      imageUrl:
+          "https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/maca-gala1-1e5fb8f95f2e6d251a15221697061927-1024-1024.jpg",
+      price: 4.99),
+  Product(
+      description:
+          "Banana prata, produzida nas terras fantásticas. Nutritiva e suculenta.",
+      id: "2",
+      name: "Banana Prata",
+      measure: "1Kg",
+      imageUrl:
+          "https://ceagesp.gov.br/wp-content/uploads/2019/12/Banana_pratapng.png",
+      price: 3.99),
+  Product(
+      description:
+          "Uva sem semente, produzida nas terras fantásticas. Nutritiva e suculenta.",
+      id: "3",
+      name: "Uva sem semente",
+      measure: "1Kg",
+      imageUrl:
+          "https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/uva-brs-isis-sem-semente1-4117fb3e4898b50a5a16102264065112-1024-1024.jpg",
+      price: 7.99),
+];
--- vish/lib/screens/home_screen.dart
+++ vish/lib/screens/home_screen.dart
@@ -1,73 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:vish/widgets/product_item.dart';
 
-import '../models/category.dart';
-import '../models/product.dart';
-import '../widgets/category_item.dart';
+import '../navigation/my_bottom_navbar.dart';
+import '../widgets/categories_list.dart';
+import '../widgets/products_list.dart';
 import '/widgets/search_input.dart';
-
-final List<Product> products = [
-  Product(
-      description:
-          "Maçã gala nacional, produzida nas terras fantásticas. Nutritiva e suculenta.",
-      id: "1",
-      name: "Maçã Gala Nacional",
-      measure: "1Kg",
-      imageUrl:
-          "https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/maca-gala1-1e5fb8f95f2e6d251a15221697061927-1024-1024.jpg",
-      price: 4.99),
-  Product(
-      description:
-          "Banana prata, produzida nas terras fantásticas. Nutritiva e suculenta.",
-      id: "2",
-      name: "Banana Prata",
-      measure: "1Kg",
-      imageUrl:
-          "https://ceagesp.gov.br/wp-content/uploads/2019/12/Banana_pratapng.png",
-      price: 3.99),
-  Product(
-      description:
-          "Uva sem semente, produzida nas terras fantásticas. Nutritiva e suculenta.",
-      id: "3",
-      name: "Uva sem semente",
-      measure: "1Kg",
-      imageUrl:
-          "https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/uva-brs-isis-sem-semente1-4117fb3e4898b50a5a16102264065112-1024-1024.jpg",
-      price: 7.99),
-  Product(
-      description:
-          "Maçã gala nacional, produzida nas terras fantásticas. Nutritiva e suculenta.",
-      id: "1",
-      name: "Maçã Gala Nacional",
-      measure: "1Kg",
-      imageUrl:
-          "https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/maca-gala1-1e5fb8f95f2e6d251a15221697061927-1024-1024.jpg",
-      price: 4.99),
-  Product(
-      description:
-          "Banana prata, produzida nas terras fantásticas. Nutritiva e suculenta.",
-      id: "2",
-      name: "Banana Prata",
-      measure: "1Kg",
-      imageUrl:
-          "https://ceagesp.gov.br/wp-content/uploads/2019/12/Banana_pratapng.png",
-      price: 3.99),
-  Product(
-      description:
-          "Uva sem semente, produzida nas terras fantásticas. Nutritiva e suculenta.",
-      id: "3",
-      name: "Uva sem semente",
-      measure: "1Kg",
-      imageUrl:
-          "https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/uva-brs-isis-sem-semente1-4117fb3e4898b50a5a16102264065112-1024-1024.jpg",
-      price: 7.99),
-];
-
-final List<Category> categories = [
-  Category(id: 1, name: "Bebidas", image: "assets/images/bebidas.png"),
-  Category(id: 2, name: "Frutas", image: "assets/images/frutas.png"),
-  Category(id: 3, name: "Grãos", image: "assets/images/graos.png")
-];
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -104,79 +40,19 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Center(child: SearchInput()),
-          const SizedBox(
+        children: const [
+          Center(child: SearchInput()),
+          SizedBox(
             height: 10,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "Categorias",
-                  style: TextStyle(
-                      fontSize: 24, color: Colors.black, fontFamily: "Acme"),
-                ),
-                TextButton(
-                    onPressed: () {},
-                    child: Text("Mostrar todas",
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                        ))),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-            child: SizedBox(
-              width: 400,
-              height: 100,
-              child: ListView.separated(
-                  separatorBuilder: (context, index) => const SizedBox(
-                        width: 15,
-                      ),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  itemBuilder: ((context, index) =>
-                      CategoryItem(categories[index]))),
-            ),
-          ),
-          const SizedBox(
+          CategoriesList(),
+          SizedBox(
             height: 25,
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
-            child: Text(
-              "Produtos",
-              style: TextStyle(
-                  fontSize: 24, color: Colors.black, fontFamily: "Acme"),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Expanded(
-            child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                child: GridView.builder(
-                    itemCount: products.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            childAspectRatio: 150 / 171,
-                            crossAxisSpacing: 10,
-                            mainAxisSpacing: 10,
-                            crossAxisCount: 2),
-                    itemBuilder: ((context, index) =>
-                        ProductItem(products[index])))),
-          ),
+          Expanded(child: ProductsList()),
         ],
       ),
+      bottomNavigationBar: const MyBottomNavBar(),
     );
   }
 }
