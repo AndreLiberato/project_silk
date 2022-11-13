8e6e984509c3d59819c323afce84b6ed6561090d
Hilton Thallyson <hilton.thallyson@hotmail.com>
2022-11-13 16:54:02 -0300

    feat-finished creating groceries list

--- vish/lib/data/groceries_lists.dart
+++ vish/lib/data/groceries_lists.dart
@@ -4,13 +4,22 @@ import '../models/list_product.dart';
 
 List<GroceryList> groceriesLists = [
   GroceryList(
-    id: "597846mnoe",
-    name: "Frutas",
-    description: "Compra de frutas semanais",
+      id: "597846mnoe",
+      name: "Frutas",
+      description: "Compra de frutas semanais",
+      listProducts: [
+        ListProduct(products[0], 5),
+        ListProduct(products[1], 5),
+        ListProduct(products[3], 5)
+      ],
+      hasAutoPayment: true,
+      paymentDate: DateTime.now()),
+  GroceryList(
+    id: "5978496mnoe",
+    name: "Cervejas",
+    description: "Compra de cerveja",
     listProducts: [
-      ListProduct(products[0], 10),
-      ListProduct(products[1], 5),
-      ListProduct(products[3], 8)
+      ListProduct(products[6], 50),
     ],
   ),
 ];
