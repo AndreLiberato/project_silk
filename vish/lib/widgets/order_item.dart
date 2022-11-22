6067290cd7591d8a1ee0870c462f499e7f8a0ff4
Hilton Thallyson <hilton.thallyson@hotmail.com>
2022-11-22 10:26:58 -0300

    fix- overflow fix at order screen

--- vish/lib/widgets/order_item.dart
+++ vish/lib/widgets/order_item.dart
@@ -14,64 +14,94 @@ class OrderItem extends StatefulWidget {
 }
 
 class OrderState extends State<OrderItem> {
-
-  late OrdersProvider myOrder = Provider.of<OrdersProvider>(context, listen: false);
+  late OrdersProvider myOrder =
+      Provider.of<OrdersProvider>(context, listen: false);
   DateTime data = DateTime.now();
 
-  @override  
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: 50,
       height: 100,
       child: ListTile(
-              onTap: () {
-                _configurandoModalBottomSheet(context);
-              },
-              leading: const Icon(Icons.shopping_bag, color: Color(0xFFf65c05),size: 35,),
-              title:  Text("#${widget.order.number.toString()}", textAlign: TextAlign.center, style: const TextStyle(color: Colors.black, fontSize: 20, fontFamily: "Acme")),
-              subtitle: Column(children: [Text(widget.order.status, style: TextStyle(color: widget.order.color, fontSize: 15, fontFamily: "Acme"), textAlign: TextAlign.start,),Text(DateFormat("d 'de' MMMM 'de' y","pt_BR").format(data), style: const TextStyle(color: Colors.grey, fontSize: 15, fontFamily: "Acme"), textAlign: TextAlign.start)]),
-              trailing: Text("R\$ ${widget.order.valor.toStringAsFixed(2)}", style: const TextStyle(color: Color(0xFFf65c05), fontSize: 20, fontFamily: "Acme"), textAlign: TextAlign.center,),
-            ),
+        onTap: () {
+          _configurandoModalBottomSheet(context);
+        },
+        leading: const Icon(
+          Icons.shopping_bag,
+          color: Color(0xFFf65c05),
+          size: 35,
+        ),
+        title: Text("#${widget.order.number.toString()}",
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                color: Colors.black, fontSize: 20, fontFamily: "Acme")),
+        subtitle: Column(children: [
+          Text(
+            widget.order.status,
+            style: TextStyle(
+                color: widget.order.color, fontSize: 15, fontFamily: "Acme"),
+            textAlign: TextAlign.start,
+          ),
+          Text(DateFormat("d 'de' MMMM 'de' y", "pt_BR").format(data),
+              style: const TextStyle(
+                  color: Colors.grey, fontSize: 15, fontFamily: "Acme"),
+              textAlign: TextAlign.start)
+        ]),
+        trailing: Text(
+          "R\$ ${widget.order.valor.toStringAsFixed(2)}",
+          style: const TextStyle(
+              color: Color(0xFFf65c05), fontSize: 20, fontFamily: "Acme"),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 
-  void _configurandoModalBottomSheet(context){
+  void _configurandoModalBottomSheet(context) {
     showModalBottomSheet(
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(20)
-        )
-      ),
-      builder: (BuildContext bc){
-        return SizedBox(
-          height: 110,
-          child: Expanded(
-            child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        leading: const Icon(Icons.check, color: Colors.green, size: 20,),
-                        title: const Text('Confirmar Entrega',style: TextStyle(color: Colors.green, fontSize: 20),),
-                        onTap: (){
+        context: context,
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+        builder: (BuildContext bc) {
+          return SizedBox(
+              height: 110,
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    child: ListTile(
+                        leading: const Icon(
+                          Icons.check,
+                          color: Colors.green,
+                          size: 20,
+                        ),
+                        title: const Text(
+                          'Confirmar Entrega',
+                          style: TextStyle(color: Colors.green, fontSize: 20),
+                        ),
+                        onTap: () {
                           setState(() {
                             myOrder.confirm(widget.order);
                           });
-                        }          
-                      ),
-                      ListTile(
-                       leading: const Icon(Icons.close, color: Colors.red, size: 20,),
-                        title: const Text('Cancelar Pedido',style: TextStyle(color: Colors.red, fontSize: 20)),
-                        onTap: (){
-                          setState(() {
-                            myOrder.cancel(widget.order);
-                          });
-                        },          
-                      ),
-                    ],
-          ))
-        );
-      }
-    );
-}
-
+                        }),
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.close,
+                      color: Colors.red,
+                      size: 20,
+                    ),
+                    title: const Text('Cancelar Pedido',
+                        style: TextStyle(color: Colors.red, fontSize: 20)),
+                    onTap: () {
+                      setState(() {
+                        myOrder.cancel(widget.order);
+                      });
+                    },
+                  ),
+                ],
+              ));
+        });
+  }
 }
